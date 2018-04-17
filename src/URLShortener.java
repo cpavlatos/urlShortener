fcc552ac39b5554a08d2d0097ceafd2e367ba104
cpavlatos <cpavlatos@dandb.com>
2018-04-17 10:52:55 -0700

    created class for validation

--- src/URLShortener.java
+++ src/URLShortener.java
@@ -1,53 +1,31 @@
 package src;
-
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.util.HashMap;
+/*
+ * Author: Xtos
+ */
 import java.util.List;
-import java.util.Map;
-import com.rosaloves.bitlyj.Url;
+import com.rosaloves.bitlyj.Bitly.Provider;
+import com.rosaloves.bitlyj.ShortenedUrl;
 import static com.rosaloves.bitlyj.Bitly.*;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.*;
-import java.util.Objects;
+
 
 public class URLShortener {
 
 	public static void main(String[] args) throws Exception {
 
 		String siteLine = null;
-		List<String> urls= ReadFromFile.urls();
+
+		List<String> urls = ReadFromFile.urls();
+		// BitlyClient client = new BitlyClient("o_1ki0u1sud9",
+		// "R_f4121ab5e7284f9fabfbef18eb7db250");
+		Provider bit = as("o_1ki0u1sud9", "R_f4121ab5e7284f9fabfbef18eb7db250");
+
 		for (int i = 0; i < urls.size(); i++) {
 			siteLine = urls.get(i);
-			Url url = as("o_1ki0u1sud9", "R_f4121ab5e7284f9fabfbef18eb7db250").call(shorten(urls.get(i)));
-			String getShortUrl = url.getShortUrl();
-			String getLongUrl = url.getLongUrl();
-			URL urlValidate = new URL(url.getShortUrl());
-			
-			HttpURLConnection urlConnection = (HttpURLConnection) urlValidate.openConnection();
-
-			urlConnection.setRequestMethod("GET");
-			urlConnection.setInstanceFollowRedirects(false);
-			HttpURLConnection.setFollowRedirects(false);
-
-			//String uriInString = getShortUrl.toString();
-
-			String newUrl = urlConnection.getHeaderField("Location");
-			System.out.println("Location : " + newUrl);
-
-			boolean testResult = passOrFail(newUrl, siteLine);
-			System.out.println(testResult);
-			urlConnection.disconnect();
-			System.out.println("short url: " + getShortUrl);
-			System.out.println("Long url: " + getLongUrl + "\n");
-			
-		}	
-	}
-	
-	public static boolean passOrFail(String newUrl, String siteLine) {
-		return (Objects.equals(siteLine, newUrl));
+			ShortenedUrl url = bit.call(shorten(urls.get(i)));
+			ValidateResponse v = new ValidateResponse();
+			v.validateShortenedUrl(url, siteLine);
 		}
-	
-}
 
+	}
+
+}
---
+++ src/ValidateResponse.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Objects;
+
+import com.rosaloves.bitlyj.ShortenedUrl;
+
+public class ValidateResponse {
+	public void validateShortenedUrl(ShortenedUrl url, String siteLine ) throws Exception{
+
+		String getShortUrl = url.getShortUrl();
+		String getLongUrl = url.getLongUrl();
+		URL urlValidate = new URL(getShortUrl);
+		HttpURLConnection urlConnection = (HttpURLConnection) urlValidate.openConnection();
+		urlConnection.setRequestMethod("GET");
+		urlConnection.setInstanceFollowRedirects(false);
+		HttpURLConnection.setFollowRedirects(false);
+
+		String newUrl = urlConnection.getHeaderField("Location");
+		System.out.println("Location : " + newUrl);
+
+		boolean testResult = ValidateResponse.passOrFail(newUrl, siteLine);
+		System.out.println(testResult);
+		urlConnection.disconnect();
+		
+		System.out.println("short url: " + getShortUrl+"\nLong url: " + getLongUrl + "\n");
+	}
+	public static boolean passOrFail(String newUrl, String siteLine) {
+		return (Objects.equals(siteLine, newUrl));
+	}
+
+}
